3d9a66dcb22714cc888ef5ed6dd78ece47c5b5ae
bogdashka-swap <bogdashka.first@gmail.com>
2025-07-19 11:38:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
